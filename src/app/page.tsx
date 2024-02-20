5698aabdc5c7371b1e0d68b7e638db5ae30d9c21
antonio <antonio109mesquita@gmail.com>
2024-02-20 00:01:40 -0300

    add responsive tailwind test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,21 @@
 export default function Home() {
   return (
-    <div className="p-4 bg-slate-900 text-slate-100 h-screen">
-      <h1 className="font-bold flex items-center gap-3
-       text-5xl before:w-0.5 before:h-8
-        before:bg-sky-500 before:flex">Hello tailwind</h1>
-      <h2>Hello tailwind</h2>
-      <p>Hello tailwind</p>
-      <button disabled className="bg-sky-500
-       px-4 py-2 rounded-md 
+    <div className="p-4 bg-slate-50 text-slate-900 dark:bg-slate-900 dark:text-slate-100 h-screen flex flex-col items-center text-center">
+      <div className="max-w-2xl">
+      <h1 className="font-bold 
+       text-4xl sm:text-5xl   ">
+        Rapidly build modern websites
+         without ever leaving your HTML.</h1>
+      <p className="mt-4 dark:text-slate-400">A utility-first CSS framework packed with classes like flex, pt-4, 
+        text-center and rotate-90 that can be composed to build any design, directly in your markup .</p>
+      <button className="bg-sky-500
+       px-4 py-2 rounded-md text-white
        font-semibold mt-4
-       enabled:hover:bg-sky-600 disabled:opacity-60 disabled:cursor-not-allowed">Sign in</button>
+       enabled:hover:bg-sky-600 
+       disabled:opacity-60 disabled:cursor-not-allowed
+       dark:bg-sky-400 dark:text-sky-950
+       ">Sign in</button>
+      </div>
     </div>
   );
 }
