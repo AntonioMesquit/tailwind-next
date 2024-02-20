ed2a6e4452063af0c99b8fcb9c49f1cbe2b59d42
antonio <antonio109mesquita@gmail.com>
2024-02-19 23:04:41 -0300

    add first commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,9 +2,7 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.tsx",
   ],
   theme: {
     extend: {
@@ -13,6 +11,10 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        antonio: '#8257e6',
+        italo: '#8245',
+      }
     },
   },
   plugins: [],
