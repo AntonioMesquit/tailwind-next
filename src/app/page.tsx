9acd01805d50e61ac95ba2f9a78b83ebd6b22663
Antonio Jose Mesquita Silveira <140577539+AntonioMesquit@users.noreply.github.com>
2024-03-04 10:32:36 -0300

    resolve bug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import { SettingTabs } from '@/components/SettingTabs';
+import { SettingTabs } from '@/components/SettingTabs'
 
 export default function Home() {
   return (
     <>
-    <h1 className="text-3xl font-medium text-zinc-900">Settings</h1>
-    <SettingTabs />
+      <h1 className="text-3xl font-medium text-zinc-900">Settings</h1>
+      <SettingTabs />
     </>
   )
 }
