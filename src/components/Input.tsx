d981dad8cb33d95c29f295692f659d6ed444b47a
Antonio Jose Mesquita Silveira <140577539+AntonioMesquit@users.noreply.github.com>
2024-03-04 09:53:04 -0300

    Input Component Composition Pattern

---
+++ src/components/Input.tsx
@@ -0,0 +1,26 @@
+import { ComponentProps } from 'react'
+
+export type InputPrefixProps = ComponentProps<'div'>
+export function InputPrefix(props: InputPrefixProps) {
+  return <div {...props} />
+}
+export type InputControlProps = ComponentProps<'input'>
+export function InputControl(props: InputControlProps) {
+  return (
+    <input
+      className="flex-1 border-0 bg-transparent p-0 text-zinc-900 placeholder-zinc-600 outline-none"
+      {...props}
+    />
+  )
+}
+export interface InputRootProps extends ComponentProps<'div'> {}
+
+export function InputRoot(props: InputRootProps) {
+  return (
+    <div
+      className="mx-1 flex w-full items-center
+     gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm "
+      {...props}
+    />
+  )
+}
--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -14,18 +14,18 @@ import { Logo } from './logo'
 import { NavItem } from './NavItem'
 import { UsedSpaceWidget } from './UserdSpaceWidget'
 import { Profile } from './Profile'
+import { InputControl, InputPrefix, InputRoot } from '../Input'
 
 export function Sidebar() {
   return (
     <aside className=" flex flex-col gap-6 border-r border-zinc-200 px-5 py-8">
       <Logo />
-      <div className="mx-1 flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm">
-        <Search className="h-5 w-5 text-zinc-500" />
-        <input
-          className="flex-1 border-0 bg-transparent p-0 text-zinc-900 placeholder-zinc-600 outline-none"
-          placeholder="Search"
-        />
-      </div>
+      <InputRoot>
+        <InputPrefix>
+          <Search className="h-5 w-5 text-zinc-500" />
+        </InputPrefix>
+        <InputControl placeholder="Search Itens" />
+      </InputRoot>
       <nav className="space-y-0.5">
         <NavItem title="Home" icon={Home} />
         <NavItem title="Dashboard" icon={BarChart} />
