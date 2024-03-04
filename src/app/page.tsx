5c42b425b811ff0bbb8025f7be4eb45fe440ff35
Antonio Jose Mesquita Silveira <140577539+AntonioMesquit@users.noreply.github.com>
2024-03-04 10:28:04 -0300

    Creating Tabs with Radix Tabs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
+import { SettingTabs } from "@/components/SettingTabs";
+
 export default function Home() {
-  return <h1>Hello world</h1>
+  return (
+    <>
+    <h1 className="text-3xl font-medium text-zinc-900">Settings</h1>
+    <SettingTabs />
+    </>
+  )
 }
