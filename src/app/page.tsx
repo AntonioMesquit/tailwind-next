c52ed8e497cedb351c07efe61fecfa13582015c2
antonio <antonio109mesquita@gmail.com>
2024-03-08 16:50:00 -0300

    fixing bug commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -90,9 +90,9 @@ export default function Home() {
               </div>
               <label
                 htmlFor="photo"
-                className="boder-zinc-300 hover:bg-violet-25 group flex flex-1 cursor-pointer flex-col items-center gap-3 rounded-lg border px-6 py-4 text-center text-zinc-500 shadow-sm hover:border-violet-200"
+                className="boder-zinc-300 group flex flex-1 cursor-pointer flex-col items-center gap-3 rounded-lg border px-6 py-4 text-center text-zinc-500 shadow-sm hover:border-violet-200 hover:bg-violet-25"
               >
-                <div className="border-6 rounded-full border-zinc-50 bg-zinc-100 p-2 group-hover:border-violet-50 group-hover:bg-violet-100">
+                <div className="rounded-full border-6 border-zinc-50 bg-zinc-100 p-2 group-hover:border-violet-50 group-hover:bg-violet-100">
                   <UploadCloud className="h-5 w-5 text-zinc-600 group-hover:text-violet-600" />
                 </div>
                 <div className="flex flex-col items-center gap-1">
